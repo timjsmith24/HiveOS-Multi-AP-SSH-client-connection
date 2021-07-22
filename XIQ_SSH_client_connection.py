0ec7b175847f2ec8da82a77bb86f0aa843f96ee0
Tim Smith <20387652+timjsmith24@users.noreply.github.com>
2021-07-22 10:10:52 -0400

    first commit

---
+++ XIQ_SSH_client_connection.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python3
+import time
+import datetime
+import sys
+import logging
+import os
+import argparse
+import paramiko
+import multiprocessing
+
+from paramiko.ssh_exception import AuthenticationException, SSHException, BadHostKeyException
+
+logging.getLogger("paramiko").setLevel(logging.WARNING)
+
+username = 'admin'
+password = 'Extreme123!'
+
+device_list = [
+    ('34.202.197.48','20106'),
+    ('34.202.197.47','20086')
+]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('clientmac', help = 'The mac address of the client you want to show connection info')
+args = parser.parse_args()
+clientmac = args.clientmac
+
+#clientmac = '0E6C82A1787E'
+
+cmd = 'show station '
+
+# Git Shell Coloring - https://gist.github.com/vratiu/9780109
+RED   = "\033[1;31m"  
+BLUE  = "\033[1;34m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+
+today = time.strftime("%Y-%m-%d")
+
+
+#-------------------------
+# logging file and info
+PATH = os.path.dirname(os.path.abspath(__file__))
+logging.basicConfig(
+    filename='{}/{}--({}).log'.format(PATH,clientmac,today),
+    filemode='a',
+    level=os.environ.get("LOGLEVEL", "INFO"),
+    #format= '%(asctime)s: %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+    format= '%(name)s - %(levelname)s - %(message)s'
+)
+
+def debug_print(msg, status):
+    if status == "error":
+        #print("ERROR: " + msg)
+        logging.error(msg)
+    elif status == "warning":
+        #print("WARNING: " + msg)
+        logging.warning(msg)
+    elif status == 'info':
+        #print("INFO: " + msg)
+        logging.info(msg)
+
+
+
+def ap_ssh(ip,port,mp_queue):
+    capturing = True
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        print (f"Establishing Connection with {ip}:{port}")
+        ssh.connect(ip,port = port, username = username , password = password ,timeout=10)
+        chan = ssh.invoke_shell()
+    except AuthenticationException:
+        print("Authentication failed on " + ip + ", please verify your credentials: %s")
+    except SSHException as sshException:
+        print("Unable to establish SSH connection on " + ip + ": %s" % sshException)
+    except BadHostKeyException as badHostKeyException:
+        print("Unable to verify server's host key on " + ip + ": %s" % badHostKeyException)
+    except Exception as e:
+        sys.stdout.write(RED)
+        sys.stdout.write("Operation error on " + ip + ": %s\n" % e)	
+        sys.stdout.write(RESET)
+    else:
+        time.sleep(1)
+        x = chan.recv(9999)
+        resp = x.decode("utf-8").splitlines()
+        devicename = resp[-1].replace('#', '')
+        chan.send('console page 0\n')
+        time.sleep(1)
+        resp = chan.recv(9999)
+        chan.send(cmd+clientmac+'\n')
+        x = chan.recv(1024)
+        clientfound = False
+        msg = ''
+        try:
+            while capturing:            
+                x = chan.recv(64)
+                if b'\r\n' == x:
+                    continue
+                if 'request station information failed because of station leave' in str(x):
+                    if clientfound == True:
+                        output = x.decode("utf-8") 
+                        now = datetime.datetime.now()
+                        mp_queue.put(f'{now.strftime("%Y-%m-%d %H:%M:%S")}: Roaming from {devicename}\t[{msg}]')
+                    clientfound = False
+                elif clientfound == False and 'request station information failed because of station leave' not in str(x) and len(x) > 50:
+                    sys.stdout.write(GREEN)
+                    sys.stdout.write(f"### Client roamed to {devicename} ###\n")
+                    sys.stdout.write(RESET)
+                    sys.stdout.flush()
+                    clientfound = True
+                    output = x.decode("utf-8") 
+                    now = datetime.datetime.now()
+                    mp_queue.put(f'{now.strftime("%Y-%m-%d %H:%M:%S")}: Roaming to {devicename}\t[{output.rstrip()}]')
+                #print(f"LENGTH - {len(x)}", end= ' ')
+                if len(x) == 0:
+                    capturing = False
+                if clientfound:
+                    output = x.decode("utf-8") 
+                    msg = output
+                    sys.stdout.write(BLUE)
+                    sys.stdout.write(f"{devicename}")
+                    sys.stdout.write(RESET)
+                    sys.stdout.write(f" {output}")
+                    sys.stdout.flush()
+            sys.stdout.write(RED)
+            sys.stdout.write(f"#### Lost connection to {devicename} ####\n")
+            sys.stdout.write(RESET)
+        except KeyboardInterrupt:
+            print(f"\n\nExiting session on {ip}:{port}")
+            return
+        ssh.close()
+
+
+
+def main():
+    sizeofbatch = 10
+    batch = device_list[0:sizeofbatch]
+    mp_queue = multiprocessing.Queue()
+    processes = []
+    for ip_address, port in batch:
+        p = multiprocessing.Process(target=ap_ssh,args=(ip_address,port, mp_queue))
+        processes.append(p)
+        p.start()
+    for p in processes:
+        try:
+            p.join()
+            p.terminate()
+        except:
+            print("\n\n#### Sessions disconnected ####")
+    mp_queue.put('STOP')
+    for line in iter(mp_queue.get, 'STOP'):
+        debug_print(line, 'info')
+    print("\n\nClosing Script")
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,47 @@
+# HiveOS Multi AP SSH client connection
+## XIQ_SSH_client_connection.py
+### Purpose
+This script will open a SSH session with multiple APs (up to 10), and run the command 'show station [MAC ADDRESS]' on each AP. The output of the AP the client is currently connected will be shown on the screen. When the client roams to a new AP the script will display a message about the roam and continue to show the statistics from the newly connected AP. This script should help provide information about client roaming. 
+
+## User Input Data
+#### SSH login info
+The username and password for the SSH session will need to be entered.
+###### lines 15-16
+```
+username = 'admin'
+password = 'Extreme123!'
+```
+#### AP info
+The IP addresses and ports of the APs to be connected to. Each set should be placed in ()'s and all but the last should be followed by a ,. 
+>###### NOTE:
+>if more than 10 APs are added to this list, SSH sessions will only be connected to the first 10
+###### lines 18-20+
+```
+device_list = [
+    ('34.202.197.48','20106'),
+    ('34.202.197.48','20059')
+]
+```
+#### client mac address
+When running the script from terminal add the mac address after the script name
+```
+python3 XIQ_SSH_client_connection.py 0E6C82A1787E
+```
+
+## Script Outputs
+#### Terminal Window
+
+<p align="center">
+<img scr="../master/images/XIQ_SSH_client_connection_output.png" alt="XIQ client connection output" height="400ox">
+</p>
+
+#### Log file
+All roams will be logged to a file [MAC ADDRESS]--([DATE]).log
+
+> 0E6C82A1787E--(2021-07-22).log
+
+This will include the last info of the client before the roam and the first info from the new AP after the roam.
+
+
+## Requirements
+This script was written for Python 3. The Paramiko module will need to be installed in order for the script to function. Paramiko is listed in the requirements.txt file.
